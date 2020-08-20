613b677a1772b4bcab7f491de4cd6d2e2acae4a5
pardeep-1212 <69990012+pardeep-1212@users.noreply.github.com>
2020-08-21 02:10:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCsDM13YqnVSydsIzZzyLfPYfTy_gTFA4w",
+  authDomain: "hacking-f2f12.firebaseapp.com",
+  databaseURL: "https://hacking-f2f12.firebaseio.com",
+  projectId: "hacking-f2f12",
+  storageBucket: "hacking-f2f12.appspot.com",
+  messagingSenderId: "847753358988",
+  appId: "1:847753358988:web:c1442be2840b6e9cd615a9",
+  measurementId: "G-9R3BFLK7YE"
 };
 
      
